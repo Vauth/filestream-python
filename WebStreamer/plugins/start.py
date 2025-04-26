5f0378bd8d8a5151faa6dabdffb3db160db11ecb
Vauth <122077080+Vauth@users.noreply.github.com>
2025-04-26 13:44:22 +0330

    UPDATE tabs-error

--- WebStreamer/plugins/start.py
+++ WebStreamer/plugins/start.py
@@ -24,11 +24,10 @@ async def start(event: NewMessage.Event):
         link_preview=False,
         parse_mode=html
     )
-    
     username = event.sender.username or "None"
-	userid = event.sender.id
-	try:
-		if userid == Var.OWNER_ID: return
-		await bot.send_message(Var.OWNER_ID, f"#START\n**Name:** {mention}\n**Username:** @{username}\n**ID:** `{userid}`")
-	except:
+    userid = event.sender.id
+    try:
+        if userid == Var.OWNER_ID: return
+        await StreamBot.send_message(Var.OWNER_ID, f"#START\n**Name:** {mention}\n**Username:** @{username}\n**ID:** `{userid}`")
+    except:
         print(f"{name} - {userid} - started me")
